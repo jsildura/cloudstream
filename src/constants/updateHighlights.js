af82811a777e3c3c90289cf5e457cfd600fc4bfd
jsildura <sildura.joelito.t@gmail.com>
2026-09-12 14:50:03 +0800

    fix: keep modal action buttons inline on mobile screens and bump version to 1.3.0

--- src/constants/updateHighlights.js
+++ src/constants/updateHighlights.js
@@ -1,4 +1,4 @@
 export const RECENT_UPDATE = {
-  version: '1.2.0',
-  highlight: 'Added movie and TV show downloads with season and episode selection and some UI improvements.',
+  version: '1.3.0',
+  highlight: 'Added movie/show episode downloads, fullscreen zoom in/out (landscape) and some UI improvements.',
 };
